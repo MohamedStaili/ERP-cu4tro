ad3eff47081a3a5e87746afbdfa56edf72ad730d
MohamedStaili <5254mohamed@gmail.com>
2025-12-03 20:46:19 +0100

    crud products done

--- erpCu4tro/products/views.py
+++ erpCu4tro/products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from accounts.permissions import IsAdmin
 
 # Create your views here.
+
+class ProductViews(viewsets.ModelViewSet):
+    queryset=Product.objects.all().order_by('-id')
+    permission_classes=[IsAdmin]
+    serializer_class=ProductSerializer
